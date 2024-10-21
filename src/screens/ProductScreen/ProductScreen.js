49ee31c724f2af13ccc1ab111107d7853cd42b34
Zunuoh <audponu@gmail.com>
2024-10-21 02:01:49 +0000

    updated routes

--- src/screens/ProductScreen/ProductScreen.js
+++ src/screens/ProductScreen/ProductScreen.js
@@ -5,7 +5,7 @@ const products = [
       id: 1,
       name: 'Zip Tote Basket',
       color: 'White and black',
-      href: '#',
+      href: 'cart',
       imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-03-related-product-01.jpg',
       imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
       price: '$140',
