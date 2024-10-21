9975a073c8a6176fb6ca70d9de4314238250918b
Zunuoh <audponu@gmail.com>
2024-10-21 01:40:28 +0000

    adjusted header

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -138,7 +138,8 @@ const HomeScreen = () => {
                   </div>
                 </div>
 
-                <a href="product" 
+               <a
+        href="/product"
                   className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
                 >
                   Shop Collection
